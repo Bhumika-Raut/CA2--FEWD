5a1ea8e8aabfce4d553c45d68aeed5d9b0e8cd03
Bhumika-Raut <144891672+Bhumika-Raut@users.noreply.github.com>
2023-11-26 09:08:33 +0530

    Update ground.js

--- Dino-game/ground.js
+++ Dino-game/ground.js
@@ -11,6 +11,7 @@ export function setupGround() {
   setCustomProperty(groundElems[0], "--left", 0)
   setCustomProperty(groundElems[1], "--left", 300)
 }
+//here the ground moves towards left followed by the width provided in the css 
 
 export function updateGround(delta, speedScale) {
   groundElems.forEach(ground => {
@@ -19,5 +20,6 @@ export function updateGround(delta, speedScale) {
     if (getCustomProperty(ground, "--left") <= -300) {
       incrementCustomProperty(ground, "--left", 600)
     }
+    //here's how another land follows to be in loop
   })
 }
